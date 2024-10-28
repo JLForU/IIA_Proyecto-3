c8c08e128a2f3640170a048c4db08933a4761444
Jhoseph Lizarazo <jhosephslizarazom@outlook.com>
2024-10-28 17:39:32 -0500

    :zap: Ya no se necesita el camino del archivo TXT, solo el nombre.

--- desarrollo/src/main/java/Main.java
+++ desarrollo/src/main/java/Main.java
@@ -9,7 +9,16 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            List<VariableLinguistica> variables = VariableLinguistica.cargarDesdeArchivo("desarrollo/src/resources/variables.txt");
+
+            // Get the class path for the resource folder dynamically
+            String classPath = String.valueOf(Main.class.getClassLoader().getResource(""));
+            classPath = classPath.replace("file:", "").replace("\\", "/");
+
+            // Construct the path to the "variables.txt" file
+            String variablesFilePath = classPath + "variables.txt";
+
+            // Load the variables from the file
+            List<VariableLinguistica> variables = VariableLinguistica.cargarDesdeArchivo(variablesFilePath);
 
             MotorInferenciaFuzzy motor = new MotorInferenciaFuzzy(variables);
 
